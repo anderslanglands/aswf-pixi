46516fe07dd40432e87dc29bd033d722ee839200
Anders Langlands <anderslanglands@gmail.com>
2026-06-24 19:48:05 +1200

    Fix MaterialX Slang consumer test headless run

--- materialx/1.39.5/tests/materialx_render_slang_consumer.cpp
+++ materialx/1.39.5/tests/materialx_render_slang_consumer.cpp
@@ -1,9 +1,14 @@
 #include <MaterialXRenderSlang/SlangRenderer.h>
 #include <MaterialXRenderSlang/TextureBaker.h>
 
+namespace
+{
+using TextureBakerFactory = MaterialX::TextureBakerSlangPtr (*)(unsigned int, unsigned int, MaterialX::Image::BaseType);
+TextureBakerFactory volatile createTextureBaker = &MaterialX::TextureBakerSlang::create;
+}
+
 int main()
 {
     MaterialX::SlangRendererPtr renderer = MaterialX::SlangRenderer::create(1, 1);
-    MaterialX::TextureBakerSlangPtr baker = MaterialX::TextureBakerSlang::create(1, 1);
-    return renderer && baker ? 0 : 1;
+    return (renderer && createTextureBaker != nullptr) ? 0 : 1;
 }
