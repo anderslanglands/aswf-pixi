f5df2f026c9e17412a01f93370445cdc0ffa9946
Anders Langlands <anderslanglands@gmail.com>
2026-06-24 17:23:47 +1200

    Fix MaterialX render CI failures

--- materialx/1.39.5/tests/materialx_render_slang_consumer.cpp
+++ materialx/1.39.5/tests/materialx_render_slang_consumer.cpp
@@ -1,7 +1,9 @@
 #include <MaterialXRenderSlang/SlangRenderer.h>
+#include <MaterialXRenderSlang/TextureBaker.h>
 
 int main()
 {
     MaterialX::SlangRendererPtr renderer = MaterialX::SlangRenderer::create(1, 1);
-    return renderer ? 0 : 1;
+    MaterialX::TextureBakerSlangPtr baker = MaterialX::TextureBakerSlang::create(1, 1);
+    return renderer && baker ? 0 : 1;
 }
